52733ee588be48e0f6e7565e8ca66d32bdc7e04b
andrey-karshkov <karshkoff@gmail.com>
2018-06-11 20:15:11 +0300

    Change credential to --passwd-stdin

--- jenkins/CI_job.groovy
+++ jenkins/CI_job.groovy
@@ -42,11 +42,9 @@ node {
 	}
 
 	stage('Push to dockerhub') {
-		withCredentials([file(credentialsId: 'dockerhub-secfile', variable: 'FILE')]) {
-            sh "docker login -u $DOCKER_HUB_USER --password-stdin < $FILE"
-            sh "docker tag $CONTAINER_NAME:$CONTAINER_TAG $DOCKER_HUB_USER/$CONTAINER_NAME:$CONTAINER_TAG"
-            sh "docker push $DOCKER_HUB_USER/$CONTAINER_NAME:$CONTAINER_TAG"
-            echo "Image push complete"
-		}
+        sh "docker login -u $DOCKER_HUB_USER --password-stdin < /var/lib/jenkins/.dockerhub-passwd"
+        sh "docker tag $CONTAINER_NAME:$CONTAINER_TAG $DOCKER_HUB_USER/$CONTAINER_NAME:$CONTAINER_TAG"
+        sh "docker push $DOCKER_HUB_USER/$CONTAINER_NAME:$CONTAINER_TAG"
+        echo "Image push complete"
 	}
 }
