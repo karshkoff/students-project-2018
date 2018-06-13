88696a72cd44e613861957c53d3b4f86150b491b
andrey-karshkov <karshkoff@gmail.com>
2018-06-13 21:00:49 +0300

    Fix tag issues

--- jenkins/CI_job.groovy
+++ jenkins/CI_job.groovy
@@ -2,11 +2,14 @@ def CONTAINER_NAME = "app"
 def CONTAINER_TAG = "latest"
 def DOCKER_HUB_USER = "karshkoff"
 def APP_HTTP_PORT = "5000"
+def IMAGE_NAME = DOCKER_HUB_USER + "/" + CONTAINER_NAME + ":" + CONTAINER_TAG
 
 node {
 
 	stage('Initialize') {
 		CONTAINER_TAG = sh(returnStdout: true, script: "git describe --tags 2>/dev/null").trim()
+		echo "Build tag: $CONTAINER_TAG"
+
         if (CONTAINER_TAG == '') {
         	currentBuild.result = 'FAILED'
 			sh "exit ${exitCode}"    
@@ -22,12 +25,12 @@ node {
 
 	stage('Build') {
 		try {
-			sh "docker rmi -f $CONTAINER_NAME"
+			sh 'docker rmi "$(docker images -q)"'
 		} catch (error) {
 		}
 
-		sh "docker build -t $CONTAINER_NAME:$CONTAINER_TAG --pull --no-cache ."
-		echo "Image $CONTAINER_NAME build complete"
+		sh "docker build -t $IMAGE_NAME --pull --no-cache ."
+		echo "Image $IMAGE_NAME build complete"
 	}
 
 	stage('Unit test') {
@@ -36,7 +39,7 @@ node {
 		} catch (error) {
 		}
 
-		sh "docker run -d --rm -p $APP_HTTP_PORT:$APP_HTTP_PORT --name $CONTAINER_NAME $CONTAINER_NAME:$CONTAINER_TAG"
+		sh "docker run -d --rm -p $APP_HTTP_PORT:$APP_HTTP_PORT --name $CONTAINER_NAME $IMAGE_NAME"
 		sleep 5
 
 		exitCode = sh(returnStatus: true, script: "docker exec app python /opt/greetings_app/test_selects.py")
@@ -49,9 +52,9 @@ node {
 	stage('Push to dockerhub') {
 		withCredentials([usernamePassword(credentialsId: 'dockerhub-karshkoff', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
 			sh "docker login -u $USERNAME -p $PASSWORD"
-			sh "docker tag $CONTAINER_NAME:$CONTAINER_TAG $DOCKER_HUB_USER/$CONTAINER_NAME:$CONTAINER_TAG"
-			sh "docker push $DOCKER_HUB_USER/$CONTAINER_NAME:$CONTAINER_TAG"
-			echo "Image push complete"
+			sh "docker tag $IMAGE_NAME $IMAGE_NAME"
+			sh "docker push $IMAGE_NAME"
+			echo "Image $IMAGE_NAME push complete"
 		}
 	}
 }
